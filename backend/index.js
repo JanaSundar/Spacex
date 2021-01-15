7043ed8eb6e224a2a2c4faced2cf9392f6b3a507
Jana <janasundar1997@gmail.com>
2021-01-15 19:13:31 +0530

    Text Compression Added

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const expressStaticGzip = require('express-static-gzip');
 
 // Application level middleware
 if (process.env.NODE_ENV === 'production') {
   const buildPath = path.join(__dirname, '..', 'build');
-  app.use(express.static(buildPath));
+  app.use(express.static(expressStaticGzip(buildPath)));
 }
 
 app.use(cors());
@@ -15,6 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // routes
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 app.use('/spacex', require('./Routes/spacex.routes'));
 
 // Connection Listener
